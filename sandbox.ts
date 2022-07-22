20ba5db0d9b4db3d028aa15b5bb7d14a6dbe2d42
Majey <kevinerick78@yahoo.com>
2022-07-22 15:15:49 +0300

    Data types

--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,19 @@
-const inputs = document.querySelectorAll("input")
-console.log(inputs)
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-inputs.forEach(input => console.log(input))
+// character = 20;
+character = 'luigi';
+
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+// console.log(area('hello'));
+console.log(circ(7.5));
