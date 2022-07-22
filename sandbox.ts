e3a28eee6d67e51fedc1b075f967d4de0be8abe1
Majey <kevinerick78@yahoo.com>
2022-07-22 16:55:49 +0300

    Functions

--- sandbox.ts
+++ sandbox.ts
@@ -1,23 +1,23 @@
-let age: any = 25;
+// let greet: Function = () => {
+//   console.log('hello, world');
+// }
 
-age = true;
-console.log(age);
-age = 'hello';
-console.log(age);
-age = { name: 'luigi' };
-console.log(age);
+// greet = 'hello';
 
-let mixed: any[] = [];
+// greet = () => {
+//   console.log('hello, again');
+// }
 
-mixed.push(5);
-mixed.push('mario');
-mixed.push(false);
-console.log(mixed);
-
-let ninja: { name: any, age: any };
-
-ninja = { name: 'yoshi', age: 25 };
-console.log(ninja);
-
-ninja = { name: 25, age: 'yoshi' };
-console.log(ninja);
+const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
+    console.log(a + b);
+    console.log(c);
+  }
+  
+  add(5, 10, 'ninja');
+  
+  const minus = (a: number, b: number): number => {
+    return a + b;
+  }
+  
+  let result = minus(10,7);
+  console.log(result);
